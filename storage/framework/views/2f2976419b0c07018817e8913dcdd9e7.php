6c80b0e648a4bc196b6a4c7180408b2379f652be
30605656 <dlamerataalumno@itec-elmolino.edu.ar>
2026-06-21 03:28:36 -0300

    feat: agregar formulario de contacto con validacion

---
+++ storage/framework/views/2f2976419b0c07018817e8913dcdd9e7.php
@@ -0,0 +1,68 @@
+
+
+<?php $__env->startSection('seccion1'); ?>
+<section>
+    <div>
+        <h1>Contacto</h1>
+
+        <?php if(session('success')): ?>
+            <p style="color: green;"><?php echo e(session('success')); ?></p>
+        <?php endif; ?>
+
+        <p>Email: <a href="mailto:danielalamerata1@gmail.com">danielalamerata1@gmail.com</a></p>
+        <p>Telefono: <a href="tel:3425233808">3425233808</a></p>
+
+        <form action="/contacto" method="POST">
+            <?php echo csrf_field(); ?>
+
+            <div>
+                <label for="nombre">Nombre</label>
+                <input type="text" id="nombre" name="nombre" value="<?php echo e(old('nombre')); ?>">
+                <?php $__errorArgs = ['nombre'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <span style="color: red;"><?php echo e($message); ?></span>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+
+            <div>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" value="<?php echo e(old('email')); ?>">
+                <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <span style="color: red;"><?php echo e($message); ?></span>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+
+            <div>
+                <label for="mensaje">Mensaje</label>
+                <textarea id="mensaje" name="mensaje"><?php echo e(old('mensaje')); ?></textarea>
+                <?php $__errorArgs = ['mensaje'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <span style="color: red;"><?php echo e($message); ?></span>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</section>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('base.base', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\proyectos\portfolio-lavarel\resources\views/contacto.blade.php ENDPATH**/ ?>
