e5d5800d01b354561aac6fb4923c61b4c9a8bf82
30605656 <dlamerataalumno@itec-elmolino.edu.ar>
2026-06-22 12:07:44 -0300

    feat: aplicar diseño retro/vintage con SCSS - cierra #9

---
+++ storage/framework/views/62b028cd0ed33b445024d1537b2293e3.php
@@ -0,0 +1,24 @@
+
+
+<?php $__env->startSection('seccion1'); ?>
+
+<section>
+    <div>
+        <h1>
+            Sobre mi
+        </h1>
+        <p>
+            Me llamo Daniela Lamerata y estoy estudiando en el itec "el molino" la carrera en Desarrollo de Software                    </p>
+        <p>
+            Desarrolladora de sitios y paginas web utilizando js, css y html
+        </p>
+        <p>
+            Aprendiendo un nuevo lenguaje php                    </p>
+        <p>
+            Aprendiendo desarrollo de backend y frontend con c# 
+        </p>
+    </div>
+</section>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('base.base', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\proyectos\portfolio-lavarel\resources\views/sobre_mi.blade.php ENDPATH**/ ?>
--- storage/framework/views/7d6c00b98adb03fd1f6ee0cec6b6d64c.php
+++ storage/framework/views/7d6c00b98adb03fd1f6ee0cec6b6d64c.php
@@ -1,12 +1,10 @@
-
-
 <?php $__env->startSection('seccion1'); ?>
 <section>
     <div>
         <h1>Proyectos</h1>
 
         <?php $__empty_1 = true; $__currentLoopData = $proyectos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $proyecto): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
-            <div>
+            <div class="proyecto-card">
                 <h2><?php echo e($proyecto->titulo); ?></h2>
                 <p><?php echo e($proyecto->descripcion); ?></p>
                 <p><strong>Tecnologías:</strong> <?php echo e($proyecto->tecnologias); ?></p>
